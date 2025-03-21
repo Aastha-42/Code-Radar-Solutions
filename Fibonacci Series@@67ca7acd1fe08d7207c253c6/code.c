1179a9b47fb94e643a381e2c97ef95cfebb86b22
Aastha Sharma <sharmaaastha2006@gmail.com>
2025-03-21 16:47:23 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,12 @@
+// Your code here...
+int fibseries(int num){
+    int a=0;
+    int b=1;
+    int c=0;
+    for(int i=0;i<n;i++){
+        printf("%d", a);
+        c=a+b;
+        a=b;
+        b=c;
+    }
+}
